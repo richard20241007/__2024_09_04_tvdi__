46786511a567a2de9f97dc55a32a79d5eb1e6df9
Ken <abcd90236@gmail.com>
2024-10-11 14:44:41 +0800

    py檔就是一個module 要先到 py檔中測試 再導入 ipynb檔案 不然會有問題

---
+++ lesson03/tools.py
@@ -0,0 +1,36 @@
+"""
+py檔就是一個model
+內建model 可以放入
+1.常數
+2.function()
+3.class
+
+
+
+
+如果用 .ipynb 呼叫 .py
+就會把 .py 記憶起來 後續就算更改
+執行 .ipynb 都還是舊版的 .py
+
+
+
+
+"""
+
+
+MON = 1
+TUE = 2
+WED = 3
+THU = 4
+FRI = 5
+SAT = 6
+SUN = 7
+
+
+
+
+
+
+
+
+
