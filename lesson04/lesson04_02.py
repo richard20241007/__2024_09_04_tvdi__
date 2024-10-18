e47290a77f104c38461d08620978dfa7320a0ac8
Ken <abcd90236@gmail.com>
2024-10-18 11:35:15 +0800

    把tkinter包在main裡面

--- lesson04/lesson04_02.py
+++ lesson04/lesson04_02.py
@@ -1,22 +1,24 @@
 import tkinter as tk
 root = tk.Tk()   #他是一個Tk的實體
 # print(type(root))
-root.title("Lesson04")
-root.geometry("500x500+25+25")
+
+def main():
+    root.title("Lesson04")
+    root.geometry("500x500+25+25")
+
+    lb1 = tk.Label(root,text='Account')
+    lb1.pack()
+
+    text = tk.StringVar()
+    ent1 = tk.Entry(root,textvariable=text,bg='yellow')
+    ent1.pack(fill='x',expand=True)
+
+    def click():
+        pass
+    btn1 = tk.Button(root,text='Click me',command=click)
+    btn1.pack()
+    root.mainloop()     #有個Tk實體方法叫做 mainloop()
 
 
-def submit():
-    pass
-
-lb1 = tk.Label(root,text='Account')
-lb1.pack()
-
-text = tk.StringVar()
-ent1 = tk.Entry(root,textvariable=text,bg='yellow')
-ent1.pack(fill='x',expand=True)
-
-btn1 = tk.Button(root,text="submit",command=submit())
-btn1.pack()
-
-
-root.mainloop()     #有個Tk實體方法叫做 mainloop()
+if __name__ =='__main__':
+    main()
