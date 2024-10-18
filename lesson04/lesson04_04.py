c3510510eafad6264a26a0476da9c107c4dded44
Ken <abcd90236@gmail.com>
2024-10-18 14:02:48 +0800

    屬性及方法

---
+++ lesson04/lesson04_04.py
@@ -0,0 +1,35 @@
+"""
+self.zzz() 叫做方法
+self.zzz= abc 叫做 屬性
+
+"""
+
+
+
+
+
+import tkinter as tk
+from tkinter import ttk
+
+
+
+class Window(tk.Tk):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        self.title('Lesson 4')
+        self.geometry('500x500')
+        message = ttk.Label(self,text="Hello Tkinter ! ~~") #建立區域變數
+        message.pack()
+
+        
+
+
+        
+
+def main():
+    window = Window()
+    window.mainloop()
+
+
+if __name__ =='__main__':
+    main()
