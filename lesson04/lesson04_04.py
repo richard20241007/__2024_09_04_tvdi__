a2439bc1570a55f7ae2370e5128b8460a150b018
Ken <abcd90236@gmail.com>
2024-10-18 14:26:57 +0800

    使用自訂style

--- lesson04/lesson04_04.py
+++ lesson04/lesson04_04.py
@@ -19,9 +19,12 @@ class Window(tk.Tk):
         self.title('Lesson 4')
         self.geometry('500x500')
         style = ttk.Style(self)
-        style.configure('TLabel',font=('Helvetica',36))
+        style.configure('TLabel',font=('Helvetica',36))  #修改現有的
+        style.configure('Title.TLabel',font=('Helvetica',20)) #自訂的style
         message = ttk.Label(self,text="Hello Tkinter ! ~~") #建立區域變數
         message.pack()
+        message2 = ttk.Label(self,text="使用自訂style",style='Title.TLabel') #使用自訂style
+        message2.pack()
         print(message.winfo_class()) # 出現 TLabel 就是其中一個theme
 
         
